989a7ec8958237b71a33eb89d9c0df3959675fdb
Karolina Jakubiak <karolinajakubiak9417@gmail.com>
2018-10-10 17:32:12 +0200

    Initial commit

---
+++ src/main/java/pl/infoshare/App.java
@@ -0,0 +1,17 @@
+package pl.infoshare;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int result =  "10 + 5";
+        String hometown = "nml";
+        System.out.print(age + "\n" + hometown);
+
+
+    }
+}
